a571a51efee2e6f2713831ce7379d4a1a5b564aa
HRegniez <henriregniez124@gmail.com>
2023-06-26 14:13:34 +0200

    update

--- FrontEnd/logedIn.js
+++ FrontEnd/logedIn.js
@@ -1,10 +1,8 @@
-
 const logInOut = document.querySelector('#login')
 const modifier = document.getElementById('modifier')
 const modal = document.createElement('div')
 const modalParent = document.getElementById('portfolio')
 var modalPage = 1
-
                                         // Is logged in ?
 if(localStorage.userId == 1){
     logInOut.innerHTML = 'logout'
@@ -29,21 +27,21 @@ async function modalInit() {
     if(modalPage == 1){
         const works = await requestWorks()
         injectWorks(works)
-        optionModalExt()
-        optionModalGalery()  
+        modalExt()
+        modalGalery()  
     }else if(modalPage == 2){
-        modalInitContent()
-        optionModalExt()
-        optionModalBack()
-    }
-         
+        modalExt()
+        modalBack()
+        addWork()
+    }         
 }
 function modalInitContent() {
     modal.innerHTML = ''
     modal.classList = ''
     if(modalPage === 1){
         modal.classList.add('modal')
-        modal.innerHTML = `
+        modal.innerHTML = 
+        `
             <div class="modal_contain">
                 <i class="fa-solid fa-xmark modal_exit"></i>
                 <h3>Galerie photo</h3>
@@ -57,28 +55,29 @@ function modalInitContent() {
     modalParent.appendChild(modal)
     }else if(modalPage === 2) {
         modal.classList.add('modal')
-        modal.innerHTML = `
-        <div class="modal_contain">
-            <i class="fa-solid fa-arrow-left modal_goback"></i>
-            <i class="fa-solid fa-xmark modal_exit"></i>
-            <h3>Ajout photo</h3>
-            <form class="modal_add-form" action="">
-                <fieldset class="modal_add-img">
-                    <i class="fa-sharp fa-regular fa-image"></i>
-                    <input type="file"
-                        id="add_image" name="add_image"
-                        accept="image/png, image/jpeg">
-                    <label for="add_image">jpg, png: 4mo max</label>
-                </fieldset>
-                <fieldset class="modal_add-details">
-                    <label for="add_titre">Titre</label>
-                    <input name="add_titre" id="add_titre" type="text">
-                    <label for="add_categorie">Categorie</label>
-                    <input name="add_categorie" id="add_categorie" type="text">
-                </fieldset>
-                <input class="modal_add-confirm" type="submit" value="Valider"></input> 
-            </form>
-        </div>
+        modal.innerHTML = 
+        `
+            <div class="modal_contain">
+                <i class="fa-solid fa-arrow-left modal_goback"></i>
+                <i class="fa-solid fa-xmark modal_exit"></i>
+                <h3>Ajout photo</h3>
+                <form class="modal_add-form" method="post" action="push">
+                    <fieldset class="modal_add-img">
+                        <i class="fa-sharp fa-regular fa-image"></i>
+                        <input type="file"
+                            id="add_image" name="add_image"
+                            accept="image/png, image/jpeg">
+                        <label for="add_image">jpg, png: 4mo max</label>
+                    </fieldset>
+                    <fieldset class="modal_add-details">
+                        <label for="add_titre">Titre</label>
+                        <input name="add_titre" id="add_titre" type="text">
+                        <label for="add_categorie">Categorie</label>
+                        <input name="add_categorie" id="add_categorie" type="text">
+                    </fieldset>
+                    <input class="modal_add-confirm" type="submit" value="Valider"></input> 
+                </form>
+            </div>
         `
         modalParent.appendChild(modal)
     }
@@ -102,9 +101,8 @@ function injectWorks(works) {
     }
     
 }  
-
             //////// close modal
-function optionModalExt() {
+function modalExt() {
     const modalExit = document.querySelector('.modal_exit')
     modal.addEventListener('click', (e) => {
         e.stopPropagation()
@@ -122,7 +120,7 @@ function optionModalExt() {
         modalPage = 1
     }
 }
-function optionModalBack() {
+function modalBack() {
     const backBtn = document.querySelector('.modal_goback')
     backBtn.addEventListener('click', (e) => {
         e.stopPropagation()
@@ -131,15 +129,13 @@ function optionModalBack() {
     })
 }
             //////// Works in gallery options ( zoom-icon/delete/edit )
-
-function optionModalGalery() {
+function modalGalery() {
     console.log()
     const modalFigures = document.querySelectorAll('.modal_figure')
     const modalImgs = document.querySelectorAll('.modal_img')
     const zoomIcons = document.querySelectorAll('.fa-arrows-up-down-left-right')
     const deleteBtns = document.querySelectorAll('.modal_delete')
     const editBtns = document.querySelectorAll('.modal_edit')
-
                 // zoom_on icon on hover
     for ( let i = 0; i < modalFigures.length; i++) {
         modalImgs[i].addEventListener('mouseover', e => {
@@ -148,7 +144,6 @@ function optionModalGalery() {
         modalImgs[i].addEventListener('mouseout', e => {
             zoomIcons[i].classList.toggle('zoom_off')
         })
-
                 // delete work
         deleteBtns[i].addEventListener('click', async (e) => {
             const response = await requestDelete(i)          //// !!! API request, body ? 
@@ -166,12 +161,32 @@ function optionModalGalery() {
         modalInit()
     })
 }
+
 ////// need to do the delete all option
 function modalClose() {
     modal.classList.remove('modal')
     modal.innerHTML = ''    
 }
+function addWork() {
+    const imgInput = document.querySelector('#add_image')
+    const titleInput = document.querySelector('#add_titre')
+    const catInput = document.querySelector('#add_categorie')
+    const form = document.querySelector('.modal_add-form')
+    const imagePreview = document.querySelector('.modal_add-img')
 
+    imgInput.addEventListener('change', (e) => {
+        const img = imgInput.files[0]
+        // if(img) {                            ////// change to image preview ????
+        //     console.log(img)
+        //     const url = img.
+        //     imagePreview.innerHTML = 
+        //     `
+        //         <img src="${url}" alt="image preview"
+        //     `
+        // }
+    })
+}
+////// Requests
 async function requestWorks() {
     try {
         const response = await fetch('http://localhost:5678/api/works')
@@ -184,7 +199,6 @@ async function requestWorks() {
         console.error('error', error)
     } 
 }   
-
 async function requestDelete(i){
     try {
         const req = await fetch(`'http://localhost:5678/api/works/${i}'`, {
